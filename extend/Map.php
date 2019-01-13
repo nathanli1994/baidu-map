84c435d8a3e2df602d4825d697d0af8e3052c471
lihenan <33186874+nathanli1994@users.noreply.github.com>
2019-01-13 13:53:42 -0500

    解决获取不到经纬度
    json_decode(),第二个参数默认是false，此时给你一个对象，如果第二个参数设置为true，则返回一个数组

--- extend/Map.php
+++ extend/Map.php
@@ -21,7 +21,7 @@ class Map{
             ];
 
             //http://api.map.baidu.com/geocoder/v2/?address=北京市海淀区上地十街10号&output=json&ak=您的ak&callback=showLocation //GET请求
-            $url = config('map.baidu_map_url') . config('map.geocoder') . '?' . 'address=' . $address . '&' . 'output=json' . '&' . 'ak=' . config('map.ak');
+            $url = config('map.baidu_map_url') . config('map.geocoder') . '?' . http_build_query($data);
 
             /*
              * 发送url获取内容
@@ -29,7 +29,8 @@ class Map{
              *  2.curl
              */
             $result = doCurl($url);
-            return $result;
+            $res = json_decode($result, true);
+            return $res;
         }
     }
 
@@ -39,6 +40,7 @@ class Map{
      * 跟进经纬度或者地址来获取对应的地图场景
      */
     public static function panorama($center){
+
         if(!$center){
             return '';
         }else{
@@ -48,9 +50,14 @@ class Map{
                 'height'    =>  config('map.height'),
                 'fov'    =>  180,
             ];
+
+            $res = Map::getLngLat($center);
+            $lng = $res['result']['location']['lng'];
+            $lat = $res['result']['location']['lat'];
+
             //http://api.map.baidu.com/panorama/v2?ak=E4805d16520de693a3fe707cdc962045&width=512&height=256&location=116.313393,40.04778&fov=180
-            //http://api.map.baidu.com/panorama/v2?ak=q0d7GGbljpt2lbSAbv0zVYS273GrX3f3&location=北京市海淀区上地十街10号&width=400&height=300&fov=180
-            $url = config('map.baidu_map_url') . config('map.panorama') . '?' . 'ak=' . config('map.ak') . '&location=' . $center . '&width=' . config('map.width') . '&height=' . config('map.height') . '&fov=' . $data['fov'];
+            //http://api.map.baidu.com/panorama/v2?ak=q0d7GGbljpt2lbSAbv0zVYS273GrX3f3&location=116.3084202915,40.057030333459&width=400&height=300&fov=180
+            $url = config('map.baidu_map_url') . config('map.panorama') . '?' . 'ak=' . config('map.ak') . '&location=' . $lng . ',' . $lat . '&width=' . config('map.width') . '&height=' . config('map.height') . '&fov=' . $data['fov'];
             /*
              * 发送url获取内容
              *  1.file_get_contents()
