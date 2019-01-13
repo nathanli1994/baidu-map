cb6406f754e43b31f07c173fe5c003d0257da24c
lihenan <33186874+nathanli1994@users.noreply.github.com>
2019-01-12 22:12:22 -0500

    app/extra
    tp自带目录，用来加载一些第三方配置项，具体操作：config('file_name.array_key')

---
+++ extra/map.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/1/12
+ * Time: 19:50
+ */
+
+/*
+ * 地图相关配置
+ */
+return [
+    'ak'    =>  'q0d7GGbljpt2lbSAbv0zVYS273GrX3f3',
+    'baidu_map_url' =>  'http://api.map.baidu.com/',
+    'geocoder'   =>  'geocoder/v2/',
+    'width' =>  400,
+    'height'    =>  300,
+    'panorama'   =>  'panorama/v2',
+];
